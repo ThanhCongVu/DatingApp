561e10f5161322fec5b3881373d99216adbb24eb
unknown <thanhcongvu.aus@gmail.com>
2019-07-18 16:50:08 +1000

    validate username/password

--- DatingApp.API/Dtos/UserForRegisterDto.cs
+++ DatingApp.API/Dtos/UserForRegisterDto.cs
@@ -1,9 +1,18 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace DatingApp.API.Dtos
 {
     public class UserForRegisterDto
     {
+        [Required]
         public string Username { get; set; }
 
+        [Required]
+        [StringLength(8, MinimumLength = 3, ErrorMessage = "You must verify password between 4 and 8 characters")]
         public string Password { get; set; }
     }
 }
+
+// We have 2 options to validate our username, either through User model or UserForRegisterDto
+// however, since in User model we have 4 parameters while only Username need to be validated
+// So that it makes more sense if we validate Username in UserForRegisterDto
